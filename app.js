3982685380d1d0738e18ce697a5c50e11aa51301
Amitrajit Das <60144525+Amitrajit31@users.noreply.github.com>
2020-09-03 22:26:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,72 @@
-const express = require("express");
+//jshint esversion: 6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
 const app = express();
-const bodyParser = require("body-parser");
-const request = require("request");
 
-var apiKey = ""; //Here your API key from Mailchimp
-var listID = ""; //Here your list id
-
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html");
-})
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
 
-app.post("/", function(req, res){
-    var firstName = req.body.firstname;
-    var lastName = req.body.lastname;
-    var email = req.body.email;
-    
-    var data = {
-        members: [
-            {
-            email_address: email,
-            status: "subscribed",
-            merge_fields: {
-                FNAME: firstName,
-                LNAME: lastName
-                }
-            }
-        ]
-    }
+app.post("/", function(req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
 
-    var jsonData = JSON.stringify(data);
-
-    var options = {
-        url: "https://us20.api.mailchimp.com/3.0/lists/" + listID,
-        method: "POST",
-        headers: {
-            "Authorization": "anagutjor " + apiKey
-        },
-        body: jsonData
-    }
-
-    request(options, function(error, response, body){
-        if(error){
-            res.sendFile(__dirname + "/failure.html");
-        } else {
-            console.log(response.statusCode);
-                if(response.statusCode === 200){
-                    res.sendFile(__dirname + "/success.html");
-                } else {
-                    res.sendFile(__dirname + "/failure.html");
-                }
+  var data = {
+    'members': [
+      {
+      email_address: email,
+      status: 'subscribed',
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
         }
-    })
-})
+      }
+    ],
+  }
 
-app.post("/failure", function(req, res){
-    res.redirect("/");
-})
+  var jsonData = JSON.stringify(data)
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Server is listening on port 3000");
-})
+  console.log(firstName, lastName, email);
 
+  // NOTE: The API KEY BELOW HAS BEEN DISABLED ON MAILCHIMP
+  //       AS THIS CODE WILL BE PUSHED TO PUBLIC GITHUB
+
+  var options = {
+    url: 'https://us17.api.mailchimp.com/3.0/lists/232bd1ab2e',
+    method: 'POST',
+    headers: {
+      'Authorization': "anish1 4b713ffe2687d651c4f33866d41163c1-us17"
+    },
+    body: jsonData
+  }
+
+  request(options, function(error, response, body) {
+    if (error) {
+      console.log(error);
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if(response.statusCode == 200){
+        res.sendFile(__dirname + "/success.html");
+      }else{
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+});
+
+app.post("/failure.html", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running");
+});
